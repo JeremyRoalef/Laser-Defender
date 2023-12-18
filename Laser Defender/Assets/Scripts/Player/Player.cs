b39edfe2ebd3cf9370b76c82b008121cbc14f0b2
JeremyRoalef <roalef944@morrisville.edu>
2023-12-17 19:07:52 -0500

    Enemies

---
+++ Laser Defender/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Vector2 moveInput;
+    [SerializeField] float fltMoveSpeed;
+
+
+
+    [SerializeField] float fltPaddingLeft;
+    [SerializeField] float fltPaddingRight;
+    [SerializeField] float fltPaddingUp;
+    [SerializeField] float fltPaddingDown;
+    Vector2 minBoundary;
+    Vector2 maxBoundary;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
+
+    void Update()
+    {
+
+        PlayerMovement();
+
+    }
+
+    void InitBounds()
+    {
+        //viewports represent the camera world space in the game.
+        //ViewportToWorldPoint sets the x and y viewport space to world space. basically, relate viewport space is converted to absolute space
+        Camera mainCamera = Camera.main;
+        minBoundary = mainCamera.ViewportToWorldPoint(new Vector2 (0,0));
+        maxBoundary = mainCamera.ViewportToWorldPoint (new Vector2 (1,1));
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+
+        Debug.Log(moveInput);
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 delta = moveInput * fltMoveSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2 ();
+
+        newPosition.x = Mathf.Clamp (transform.position.x + delta.x, minBoundary.x + fltPaddingLeft, maxBoundary.x - fltPaddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBoundary.y + fltPaddingDown, maxBoundary.y - fltPaddingUp);
+
+        transform.position = newPosition;
+    }
+
+}
